381641d185c6f7b8affcc40db43ba537586c0548
Soumya Biswas <soumyabiswas250498@gmail.com>
2025-02-21 11:07:31 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <div className="bg-slate-400">
+      <Button>Click me</Button>
+    </div>
+  );
+}
+
+export default App;
